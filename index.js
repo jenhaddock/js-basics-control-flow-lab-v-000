9997b60c25a5a8a0bddc75026af5be8c66a64ec3
Jennifer Haddock <jentx123@hotmail.com>
2017-11-02 02:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function scuberGreetingForFeet(distance){
   if (distance > 2500){
     return 'No can do.';
   }
+  else if (distance < 2500 && distance > 400){
+    return 'I will gladly take your thirty bucks.';
+  }
   else if (distance <= 400) {
   return 'This one is on me!';
   }
