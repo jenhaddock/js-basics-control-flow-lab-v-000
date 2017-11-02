706eb2eeed9801a96868505cc9351543b22b22cf
Jennifer Haddock <jentx123@hotmail.com>
2017-11-02 01:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance){
   if (distance <= 400) {
-  console.log("This one is on me!");
-}
+  return 'This one is on me!';
+  }
 }
